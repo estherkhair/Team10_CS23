0cf5376a3dd53ade588568088ad3065e603c40c7
OstrichLord2 <August.Kittleson@tufts.edu>
2024-10-07 22:25:41 -0400

    minor tweaks

--- Team10_CultGame/Assets/Scripts/ButtonScript.cs
+++ Team10_CultGame/Assets/Scripts/ButtonScript.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 public class ButtonScript : MonoBehaviour
 {
     public void RestartGame(){
-                SceneManager.LoadScene("Level1");
+                SceneManager.LoadScene("Level 1");
         }
 
     public void QuitGame(){
