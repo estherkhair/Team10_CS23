5e2560730f735659672599913e89229a644f3b3a
OstrichLord2 <August.Kittleson@tufts.edu>
2024-10-13 18:13:30 -0400

    Almost done with mechanics

--- Team10_CultGame/Assets/Scripts/FollowerPlant.cs
+++ Team10_CultGame/Assets/Scripts/FollowerPlant.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FollowerPlant : MonoBehaviour{
     public GameObject plantStage1;
@@ -8,6 +9,8 @@ public class FollowerPlant : MonoBehaviour{
     public GameObject plantStage3;
     public GameObject plantStage4;
     public GameObject followerPrefab;
+    public bool isWatered = false;
+    public bool isHarvested = false; 
 
 
     public float timeToGrow = 1.5f;
@@ -19,14 +22,12 @@ public class FollowerPlant : MonoBehaviour{
         plantStage2.SetActive(false);
         plantStage3.SetActive(false);
         plantStage4.SetActive(false);
-
-        // StartCoroutine(GrowPlant());
     }
-
 
     // Update is called once per frame
     IEnumerator GrowPlant(){
-        yield return new WaitForSeconds(timeToGrow);
+        if (isWatered) {
+            yield return new WaitForSeconds(timeToGrow);
     //after delay, switch to stage 2
         plantStage1.SetActive(false);
         plantStage2.SetActive(true);
@@ -37,7 +38,8 @@ public class FollowerPlant : MonoBehaviour{
         plantStage2.SetActive(false);
         plantStage3.SetActive(true);
         yield return new WaitForSeconds(timeToGrow);
-//after delay, spawn follower:
+        }
+        if (isHarvested) {
         Instantiate(followerPrefab, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(timeToGrow);
 //after delay, switch to stage 4 (whither)
@@ -45,7 +47,16 @@ public class FollowerPlant : MonoBehaviour{
         plantStage4.SetActive(true);  
         yield return new WaitForSeconds(timeToGrow);      
 //after delay, destroy (comment this line out if you want to not be abel to plant in the same place twice)
-        //Destroy(gameObject);
+        //Destroy(gameObject); */
+        }
+}
+    public void WaterPlant() {
+        isWatered = true;
+        StartCoroutine(GrowPlant());
+    }
+    public void toHarvest() {
+        isWatered = false;
+        isHarvested = true;
+        StartCoroutine(GrowPlant());
     }
 }
-
